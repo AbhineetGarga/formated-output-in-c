1916f479d0b4c990ad34410bfa246327e0cd5174
AbhineetGarga <coderboiiye@gmail.com>
2024-02-10 21:26:00 +0530

    first commit

---
+++ formated-output.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	float c,d;
+//	char e,f, sum;
+	
+	a=25;
+	b=a/2;
+	printf("%d\n",b);
+	printf("%03d\n",b); //print the number with thre blank space
+	
+	c=15.3;
+	d=c/3;
+	printf("%3.2f\n", d); //need to understand
+	
+//Please solve this	
+//	e= 'Abhneet';
+//	f= 'Garga' ;
+//	sum= e+f;
+//	printf("%d\n", sum);
+}
